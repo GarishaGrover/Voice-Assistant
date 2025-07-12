fa8ed9fd27b9d35e43f87a61db3ea608a29e2617
GarishaGrover <garishagrover0803@gmail.com>
2025-07-12 13:44:50 +0530

    Add files via upload

---
+++ pip installer.py
@@ -0,0 +1,13 @@
+pip install wolframalpha
+pip install wikipedia
+pip install ecapture
+pip install twilio
+pip install beautifulsoup4
+pip install winshell
+pip install pyjokes
+pip install feedparser
+pip install clint
+pip install SpeechRecognition pyttsx3
+pip install PyAudio
+pip install setuptools
+
